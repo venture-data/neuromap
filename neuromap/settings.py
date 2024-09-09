6e5c579bd6ba22fd3f39b09f2e9775d9546d9e6a
venture-data <137489725+venture-data@users.noreply.github.com>
2024-09-10 01:46:41 +0500

    Update settings.py

--- neuromap/settings.py
+++ neuromap/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gf&#5h++g(%veor_(1kz0&#@u^24n+p@fm4mm#la)$-k)&8(+j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
